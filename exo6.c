67d5ed69210ecb0e18c04982748eedbdf70a682a
assadMus47 <assadkac47@gmail.com>
2024-10-10 09:30:36 +0100

    third commit

---
+++ exo6.c
@@ -0,0 +1,21 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int main () {
+    int n,m,s=0,p=1,i;
+    float m;
+    i=1;
+    do{ 
+        printf("Entrez %d: ", i);
+        scanf("%d", &m);
+        s+=m;
+        p*=m;
+        i++;
+    } while(i<=n);
+    m=(float)s/n;
+    printf("s : %d\n", s);
+    printf("p : %d\n", p);
+    printf("m : %f\n", m);
+
+    return 0;
+}
