fa7da2dec74dfbf45d5b12f87be46bf3a0157eeb
assadMus47 <assadkac47@gmail.com>
2024-10-10 11:37:42 +0100

    five commit : im in the third task

--- exo2/exo2-3.c
+++ exo2/exo2-3.c
@@ -2,11 +2,58 @@
 #include <stdlib.h>
 #include <math.h>
 
-int main (){
+void to_character(int n) {
+    int a, b, o, f;
+    a = n / 100; // Hundreds place
+    b = n % 100; // Last two digits
+    o = b / 10;  // Tens place
+    f = b % 10;  // Units place
+
+    char *chaine1[9] = {
+        "un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf"
+    };
+    char *chaine2[10] = {
+        "dix", "vingt", "trente", "quarante", "cinquante", "soixante", "soixante-dix", "quatre-vingt", "quatre-vingt-dix"
+    };
+
+    if (a > 0) {
+        a==1 ? printf("cent ", chaine1[a - 1]) : printf("%s cents ", chaine1[a - 1]);
     
     
-    
+        if (o > 0) {
+        printf("%s ", chaine2[o - 1]);
+        }
+
+        if (f > 0) {
+        printf("%s", chaine1[f - 1]);
+        }
+
+        printf("\n");
+    }
+    else { 
+        if (o > 0) {
+        printf("%s ", chaine2[o - 1]);
+        }
+
+        if (f > 0) {
+            if ((o==9 || o ==7)&&(f==1))
+                printf("onze");
+            else 
+                printf("%s", chaine1[f - 1]);
+
+
+        printf("\n");
+    }
+}
+}
+
+int main() {
+    to_character(71);
+    to_character(123);
+    to_character(999);
+    to_character(100);
+    to_character(101);
+    to_character(12);
     
     return 0;
-
 }
